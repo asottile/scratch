25752fb8639ba078a40253e2ee5327bf35302b40
Anthony Sottile <asottile@umich.edu>
2015-04-10 17:47:13 -0700

    Add staticproperty

---
+++ python/staticproperty.py
@@ -0,0 +1,20 @@
+class staticproperty(object):
+    def __init__(self, fget):
+        self.fget = fget
+    def __get__(self, obj, owner):
+        return staticmethod(self.fget).__get__(None, owner)()
+
+
+class Foo(object):
+    @staticproperty
+    def bar():
+        print('hai')
+        return 9001
+
+
+print(Foo.bar)
+
+output = '''
+hai
+9001
+'''
