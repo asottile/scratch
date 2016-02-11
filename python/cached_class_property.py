88d572f42c82227d26f93728b75a0d095dd3fe36
Anthony Sottile <asottile@umich.edu>
2016-02-10 17:23:17 -0800

    Add cached static/class properties

---
+++ python/cached_class_property.py
@@ -0,0 +1,38 @@
+class cached_class_property(object):
+    def __init__(self, fget):
+        self.fget = fget
+
+    def __get__(self, obj, owner):
+        val = classmethod(self.fget).__get__(None, owner)()
+        setattr(owner, self.fget.__name__, val)
+        return val
+
+
+class Test(object):
+    @cached_class_property
+    def class_property(cls):
+        print('Called class_property')
+        return object()
+
+
+assert Test.class_property is Test.class_property
+
+
+class cached_static_property(object):
+    def __init__(self, fget):
+        self.fget = fget
+
+    def __get__(self, obj, owner):
+        val = staticmethod(self.fget).__get__(None, owner)()
+        setattr(owner, self.fget.__name__, val)
+        return val
+
+
+class Test2(object):
+    @cached_static_property
+    def static_property():
+        print('Called static_property')
+        return object()
+
+
+assert Test2.static_property is Test2.static_property
