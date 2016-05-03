e7f7a549fb3d3a78fc9985abdbc56070781ecfce
Anthony Sottile <asottile@umich.edu>
2016-05-03 08:23:43 -0700

    Make prune-remote-branches more clever about remotes

--- python/prune-remote-branches.py
+++ python/prune-remote-branches.py
@@ -1,30 +1,43 @@
 #!/usr/bin/env python3.5
 import argparse
+import getpass
 import subprocess
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--dry-run', action='store_true')
-    parser.add_argument('--remote', default='origin')
+    parser.add_argument('--remote')
+    parser.add_argument('--upstream')
     args = parser.parse_args()
 
     remotes = subprocess.check_output(
         ('git', 'remote')
     ).decode('UTF-8').splitlines()
-    if 'canon' in remotes:
+    if args.upstream:
+        upstream = args.upstream
+    elif 'canon' in remotes:
         upstream = 'canon'
     elif 'upstream' in remotes:
         upstream = 'upstream'
     elif 'origin' in remotes:
         upstream = 'origin'
     else:
-        raise AssertionError('Which remote? {}'.format(remotes))
+        raise AssertionError('Which upstream? {}'.format(remotes))
 
     if args.dry_run:
         dry_run = 'echo '
     else:
         dry_run = ''
+
+    if args.remote:
+        remote = args.remote
+    elif getpass.getuser() in remotes:
+        remote = getpass.getuser()
+    elif 'origin' in remotes:
+        remote = 'origin'
+    else:
+        raise AssertionError('Which remote? {}'.format(remotes))
 
     subprocess.check_call((
         'bash', '-c',
