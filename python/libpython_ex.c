306258b4b0cbdecf4bf8b48ed906460fcf7c9c22
Anthony Sottile <asottile@umich.edu>
2017-01-31 14:19:31 -0800

    Oops. sys.path not sys.modules

--- python/libpython_ex.c
+++ python/libpython_ex.c
@@ -11,14 +11,14 @@ static void add_empty_string_to_path() {
     /* Do this to import from the current directory */
     PyObject* sys = PyImport_ImportModule("sys");
     assert(sys);
-    PyObject* sys_modules = PyObject_GetAttrString(sys, "modules");
-    assert(sys_modules);
+    PyObject* sys_path = PyObject_GetAttrString(sys, "path");
+    assert(sys_path);
     PyObject* empty_string = PyUnicode_FromString("");
     assert(empty_string);
-    PyList_Insert(sys_modules, 0, empty_string);
+    PyList_Insert(sys_path, 0, empty_string);
 
     Py_DECREF(empty_string);
-    Py_DECREF(sys_modules);
+    Py_DECREF(sys_path);
     Py_DECREF(sys);
 }
 
