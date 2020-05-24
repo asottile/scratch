1b81a438915a423c891886fb869ef6eca55a08c5
Anthony Sottile <asottile@umich.edu>
2020-05-24 14:42:59 -0700

    use specific noqa

--- .pythonrc.py
+++ .pythonrc.py
@@ -1,5 +1,5 @@
 # Add the following to ~/.bash_profile
 # export PYTHONSTARTUP=~/.pythonrc.py
 import readline
-import rlcompleter  # noqa
+import rlcompleter  # noqa: F401
 readline.parse_and_bind('tab: complete')
