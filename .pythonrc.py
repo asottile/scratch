1c8464e17dd636b66d7ac55545e11974d3fb312c
Anthony Sottile <asottile@umich.edu>
2020-12-31 11:08:17 -0800

    add pp(...) function

--- .pythonrc.py
+++ .pythonrc.py
@@ -1,5 +1,11 @@
 # Add the following to ~/.bash_profile
 # export PYTHONSTARTUP=~/.pythonrc.py
+import pprint
 import readline
-import rlcompleter  # noqa: F401
+import rlcompleter  # imported purely for side-effects
+
 readline.parse_and_bind('tab: complete')
+del readline, rlcompleter
+
+pp = pprint.pprint
+del pprint
