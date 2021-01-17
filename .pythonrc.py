e349fd5d4b7fbcb2654da800dc31792c703ddcd0
Anthony Sottile <asottile@umich.edu>
2021-01-17 12:35:40 -0800

    add pp / ppj to pythonrc

--- .pythonrc.py
+++ .pythonrc.py
@@ -1,11 +1,17 @@
 # Add the following to ~/.bash_profile
 # export PYTHONSTARTUP=~/.pythonrc.py
-import pprint
 import readline
 import rlcompleter  # imported purely for side-effects
 
 readline.parse_and_bind('tab: complete')
 del readline, rlcompleter
 
-pp = pprint.pprint
-del pprint
+
+def pp(o):
+    import pprint
+    pprint.pprint(o)
+
+
+def ppj(o):
+    import json
+    print(json.dumps(o, indent=2))
